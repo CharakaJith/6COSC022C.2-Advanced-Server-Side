c8e1ef706eaec2222a63bb470369dadb652ab619
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-03-15 09:46:56 +0530

    week 08 - feature - initial ui implemented

--- Week_08/app.js
+++ Week_08/app.js
@@ -1,1 +1,23 @@
-console.log('Hello world...');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// serve static files from public folder
+app.use(express.static('public'));
+
+// middleware to log called endpoints
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} | ${new Date()}`);
+  next();
+});
+
+const ENV = process.env.ENV || 'development';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`${ENV} | ${PORT}`);
+});
